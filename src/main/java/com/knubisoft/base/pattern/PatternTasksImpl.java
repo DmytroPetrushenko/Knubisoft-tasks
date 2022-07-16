2ccbbb87ef40fbe9083ee347abc218c4b6f39ebd
Dmytro Petrushenko <dmytro.petrushenko@gmail.com>
2022-07-16 20:49:16 +0300

    PatternTasks were done

--- src/main/java/com/knubisoft/base/pattern/PatternTasksImpl.java
+++ src/main/java/com/knubisoft/base/pattern/PatternTasksImpl.java
@@ -1,9 +1,18 @@
 package com.knubisoft.base.pattern;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PatternTasksImpl implements PatternTasks {
 
     @Override
     public boolean haveSetOfCharacters(String text) {
+        if (text == null || text.isBlank()) {
+            throw new IllegalArgumentException();
+        }
         return text.matches("[a-zA-Z\\d]*");
     }
 
@@ -34,41 +43,68 @@ public class PatternTasksImpl implements PatternTasks {
 
     @Override
     public String matchVowels(String text) {
-        return null;
+        if (text == null || text.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        return text.replaceAll("[QEYUIOAqeyuioa]", "");
     }
 
     @Override
     public boolean validatePIN(String text) {
-        return false;
+        if (text == null || text.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        return text.matches("(\\b(\\d{4})|(\\d{6})|(\\d{8})\\b)");
     }
 
     @Override
     public String divideDigit(int digit) {
-        return null;
+        String value = String.valueOf(digit);
+        return value.replaceAll("000\\b", "#000");
     }
 
     @Override
     public String removeAllNonAlphanumericCharacters(String text) {
-        return null;
+        return text.replaceAll("(([^a-zA-z])|(\\^))", "");
     }
 
     @Override
     public boolean validatePhoneNumber(String text) {
-        return false;
+        if (text == null || text.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        return text
+                .matches("((\\(\\d{3}\\)\\d{3}-?\\d{4})|(\\d{3}-?\\d{3}-?\\d{4}))");
     }
 
     @Override
     public String getLastVowelsByConstraint(String text, int n) {
-        return null;
+        if (text == null || text.isBlank() || n < 1 || text.length() < n) {
+            throw new RuntimeException();
+        }
+        String value = text.replaceAll("[wrtpsdfghklzxcvbnmjWRTPSDFGHJKLZXCVBNM]", "");
+        return value.toLowerCase().substring(value.length() - n);
     }
 
     @Override
     public boolean isMathematicalExpression(String text) {
-        return false;
+        if (text == null || text.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        return text.matches("(\\d+(\\s[+-/%*]\\s\\d+)*)");
     }
 
     @Override
     public String insertDash(String text) {
-        return null;
+        Pattern pattern = Pattern.compile("\\b[A-Z]");
+        Matcher matcher = pattern.matcher(text);
+        List<Integer> places = new ArrayList<>();
+        while (matcher.find()) {
+            places.add(matcher.end());
+        }
+        for (int i = 0; i < places.size(); i++) {
+            text = text.substring(0, places.get(i) + i) + "-" + text.substring(places.get(i) + i);
+        }
+        return text;
     }
 }
